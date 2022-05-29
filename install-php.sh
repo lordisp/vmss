2f322b46a0d55f10299dee6f5e8a98f0ddb3f7f2
Rafael Camison <rafael.camison@austrian.com>
2022-05-30 00:48:22 +0200

    update script

--- install-php.sh
+++ install-php.sh
@@ -1,6 +1,6 @@
-# Install PHP 8.1
-sudo apt update && sudo apt -y upgrade
 #!/bin/bash
+# Install PHP 8.1
+sudo apt update -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt install php8.1 -y
