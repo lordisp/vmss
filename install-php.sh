1e2c071e4a1854c180c4beeba17b847944a7a792
Rafael Camison <rafael.camison@austrian.com>
2022-05-31 13:51:30 +0200

    replace php version with variable and add code coverage settings in php.ini

--- install-php.sh
+++ install-php.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-# Install PHP 8.1
+PHPVERSION="8.1"
+# Install PHP $(phpversion)
 sudo apt update -y
 sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:ondrej/php -y
-sudo apt install php8.1 -y
+sudo apt install php$(phpversion) -y
 
 # Installing PHP Extensions
-sudo apt install php8.1-dev php8.1-xdebug php8.1-bcmath php8.1-bz2 php8.1-cli php8.1-curl php8.1-common php8.1-dba php8.1-gd php8.1-mcrypt php8.1-mysql php8.1-psr php8.1-redis php8.1-sqlite3 php8.1-uuid php8.1-xdebug php8.1-xml php8.1-zip autoconf automake -y
+sudo apt install php$(phpversion)-dev php$(phpversion)-xdebug php$(phpversion)-bcmath php$(phpversion)-bz2 php$(phpversion)-cli php$(phpversion)-curl php$(phpversion)-common php$(phpversion)-dba php$(phpversion)-gd php$(phpversion)-mcrypt php$(phpversion)-mysql php$(phpversion)-psr php$(phpversion)-redis php$(phpversion)-sqlite3 php$(phpversion)-uuid php$(phpversion)-xdebug php$(phpversion)-xml php$(phpversion)-zip autoconf automake -y
+
+# Enable Code Coverage
+echo "xdebug.mode=coverage" >> "/etc/php/$(phpversion)/cli/php.ini"
+
+# Installing kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
