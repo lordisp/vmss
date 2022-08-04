919e4759fda211a2c6b0424a0b3ae36a3e253205
Rafael Camison <rafaelcamison@gmail.com>
2022-08-04 03:01:03 +0200

    install mbstring extension

--- install-php.sh
+++ install-php.sh
@@ -6,7 +6,7 @@ sudo add-apt-repository ppa:ondrej/php -y
 sudo apt install php8.1 -y
 
 # Installing PHP Extensions
-sudo apt install php8.1-dev php8.1-xdebug php8.1-bcmath php8.1-bz2 php8.1-cli php8.1-curl php8.1-common php8.1-dba php8.1-gd php8.1-mcrypt php8.1-mysql php8.1-psr php8.1-redis php8.1-sqlite3 php8.1-uuid php8.1-xdebug php8.1-xml php8.1-zip sqlite autoconf automake -y
+sudo apt install php8.1-dev php8.1-xdebug php8.1-bcmath php8.1-mbstring php8.1-bz2 php8.1-cli php8.1-curl php8.1-common php8.1-dba php8.1-gd php8.1-mcrypt php8.1-mysql php8.1-psr php8.1-redis php8.1-sqlite3 php8.1-uuid php8.1-xdebug php8.1-xml php8.1-zip sqlite autoconf automake -y
 
 # Enable Code Coverage
 echo "xdebug.mode=coverage" >> "/etc/php/8.1/cli/php.ini"
